31bbd3a960c553196823ece575d9a0e4fd37b1b7
pachakra <pachakra@yahoo.com>
2018-03-18 23:12:35 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo
 {
  public static void main(String [] args) {
-  System.out.println("Hello World. It was great to be in GIT");
+  System.out.println("Hello World. It was great to be in GIT. Thanks for the warm welcome");
  
   }
   
